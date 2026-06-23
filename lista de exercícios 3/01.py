32fc9eace7ee0d04ea9152c9c5d23bb2901da750
HeloisaFurio <heloisa.fc14@aluno.ifsc.edu.br>
2026-06-23 14:50:27 -0300

    .

--- lista de exercícios 3/01.py
+++ lista de exercícios 3/01.py
@@ -1,11 +1,21 @@
-idades = [0]*6
-i=0
+idades = []
 
-while i < 6:
-    idades[i] =  int(input(f"Insira a idade do aluno{i}: "))
-    i += 1
+for i in range(6):
+    idade = (int(input(f"digite aluno{i+1}: ")))
+    idades.append(idade)
 
-i=0
-if idades[i] >= 16:
-    print (f"O aluno{i} tem mais de 16 anos, ele tem {idades[i]} anos.")
-    i += 1
+for i, idade in enumerate(idades):
+    if idade >= 16:
+        print (f"O aluno{i+1} tem mais de 16 anos, ele tem {idade} anos.")
+
+
+
+# i = 0
+# while i < 6:
+#     idades[i] =  int(input(f"Insira a idade do aluno{i}: "))
+#     i += 1
+
+# i=0
+# if idades[i] >= 16:
+#     print (f"O aluno{i} tem mais de 16 anos, ele tem {idades[i]} anos.")
+#     i += 1
